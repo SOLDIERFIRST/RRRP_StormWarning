e76d14820734d077caf37b1461a5d57f46c09672
SOLDIERFIRST <74333124+SOLDIERFIRST@users.noreply.github.com>
2023-05-10 16:26:03 -0400

    Update config.lua

--- config.lua
+++ config.lua
@@ -6,5 +6,3 @@ Config.SecondWeather = "thunderstorm" -- I recommend thunderstorm for max drama
 
 Config.WindSpeed = "150" -- don't go higher than this
 Config.WindDirection = "0" -- wind heading - changes the direction the rain and trees will be pushed
-
-Config.Winter = false -- change to true for snow on the ground
