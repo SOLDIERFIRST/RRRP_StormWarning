f3f6db307d14850563ccd98aab92bb713c455093
SOLDIERFIRST <74333124+SOLDIERFIRST@users.noreply.github.com>
2023-05-10 15:58:14 -0400

    Update config.lua
    added 3rd stage weather change

--- config.lua
+++ config.lua
@@ -1,6 +1,7 @@
 Config = {}
 
 Config.FirstWeather = "hurricane" -- make sure this matches a weather type in weathersync > config.lua, this will be the starting weather
+Config.SecondWeather = "rain" -- I recommend rain for max drama
 Config.SecondWeather = "thunderstorm" -- I recommend thunderstorm for max drama
 
 Config.WindSpeed = "150" -- don't go higher than this
